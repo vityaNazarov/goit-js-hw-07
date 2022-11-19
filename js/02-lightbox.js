72a2a5f09f1f5c3e91ce90328aac35c4557a9613
Viktor <108054291+vityaNazarov@users.noreply.github.com>
2022-11-19 20:35:19 +0200

    simple-light-box

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const refs = {
+  gallery: document.querySelector(".gallery"),
+};
 
-console.log(galleryItems);
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+  <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+</li>`;
+    })
+    .join("");
+}
+refs.gallery.insertAdjacentHTML("beforeend", createGalleryMarkup(galleryItems));
+
+const gallery = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+  overlayOpacity: 0.8,
+});
+preventDefault();
